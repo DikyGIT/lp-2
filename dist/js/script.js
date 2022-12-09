adf83954a30582ace49999255d580570f646c1d6
DikyGIT <dikyoga27@gmail.com>
2022-12-09 14:05:48 +0700

    landing page 2

---
+++ dist/js/script.js
@@ -0,0 +1,6 @@
+const navBar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", () => {
+  const windowPosition = window.scrollY > 0;
+  navBar.classList.toggle("scrolling-active", windowPosition);
+});
